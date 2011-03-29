f893e5ab59b514772e2e151c2178326399acf761
August Gustavsson <ugge@doctor.com>
2011-03-29 20:18:09 +0200

    Fix 64-bit compile errors

--- src/crypto51/misc.h
+++ src/crypto51/misc.h
@@ -142,7 +142,7 @@ inline unsigned int GetAlignment(T *dummy=NULL)	// VC60 workaround
 
 inline bool IsAlignedOn(const void *p, unsigned int alignment)
 {
-	return IsPowerOf2(alignment) ? ModPowerOf2((unsigned int)p, alignment) == 0 : (unsigned int)p % alignment == 0;
+	return IsPowerOf2(alignment) ? ModPowerOf2((uintptr_t)p, alignment) == 0 : (uintptr_t)p % alignment == 0;
 }
 
 template <class T>
